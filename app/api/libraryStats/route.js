070310906a96d09f6a76ce4ae858abbdeedeeb9f
Ziv Behar <zivbeh@gmail.com>
2024-10-20 16:44:08 -0700

    again

--- app/api/libraryStats/route.js
+++ app/api/libraryStats/route.js
@@ -2,7 +2,7 @@
 import fs from 'fs';
 import path from 'path';
 
-export default async function GET(req, res) {
+export async function GET(req, res) {
     const filePath = path.join(process.cwd(), 'data', 'libraryStats.json');
     const fileContents = fs.readFileSync(filePath, 'utf8');
     res.status(200).json(JSON.parse(fileContents));
