bec50d623f4a7ba4af5c9fc6f8fb1c4eccf2337f
Ziv Behar <zivbeh@gmail.com>
2024-10-20 14:41:17 -0700

    fixedError

--- app/api/saveData/route.js
+++ app/api/saveData/route.js
@@ -42,7 +42,7 @@ export async function POST(req) {
         // });
 
         // Respond with a success message
-        return NextResponse.json({ message: newLibraryStat });
+        return NextResponse.json({ message: "newLibraryStat" });
     } catch (error) {
         console.error('Error saving data:', error);
         return NextResponse.json({ message: 'Error saving data.' }, { status: 500 });
