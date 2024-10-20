8f8ed9dcdff88bc871949e08234772cbf442da33
Ziv Behar <zivbeh@gmail.com>
2024-10-20 16:30:58 -0700

    json work

---
+++ app/api/libraryStats.js
@@ -0,0 +1,9 @@
+// pages/api/libraryStats.js
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+    const filePath = path.join(process.cwd(), 'data', 'libraryStats.json');
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    res.status(200).json(JSON.parse(fileContents));
+}
