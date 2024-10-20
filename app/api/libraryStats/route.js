d9d054d01101a406cccaf9cabcd7063e48ec1a14
Ziv Behar <zivbeh@gmail.com>
2024-10-20 16:58:26 -0700

    fixedd

--- app/api/libraryStats/route.js
+++ app/api/libraryStats/route.js
@@ -1,9 +1,10 @@
-// pages/api/libraryStats.js
+// app/api/libraryStats/route.js
 import fs from 'fs';
 import path from 'path';
+import { NextResponse } from 'next/server';
 
-export async function GET(req, res) {
+export async function GET() {
     const filePath = path.join(process.cwd(), 'data', 'libraryStats.json');
     const fileContents = fs.readFileSync(filePath, 'utf8');
-    res.status(200).json(JSON.parse(fileContents));
+    return NextResponse.json(JSON.parse(fileContents));
 }
