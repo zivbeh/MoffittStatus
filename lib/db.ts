c8d5a0f6251ff93e7b6d44e8580e308983c2887e
Ziv Behar <zivbeh@gmail.com>
2024-10-22 13:16:39 -0700

    new ui

--- lib/db.ts
+++ lib/db.ts
@@ -1,23 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
+// lib/db.ts
 import mysql from 'mysql2/promise';
 
-// Extending the global type to include _connection
-declare global {
-  // Add _connection property to globalThis
-  var _connection: mysql.Pool | undefined;
-}
-
-let connection: mysql.Pool;
-
-if (!global._connection) {
-  global._connection = mysql.createPool({
+const connection = mysql.createPool({
     uri: process.env.JAWSDB_URL || "mysql://kqydyq0eznrb573j:c4ums7k6uohxb5ua@q68u8b2buodpme2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/a8ii54ul8zyr1yt1", // Use your connection string from config vars
-    connectionLimit: 1000,          // Adjusted connection limit
-  });
-}
-
-connection = global._connection;
+    connectionLimit: 1000,
+});
 
 export default connection;
