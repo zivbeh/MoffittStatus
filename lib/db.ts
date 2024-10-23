6b5f343491e33b787cb607d2bd606d72f79f71cf
Ziv Behar <zivbeh@gmail.com>
2024-10-23 15:35:18 -0700

    updated to a reasonable connectionLimit for db

--- lib/db.ts
+++ lib/db.ts
@@ -11,7 +11,7 @@ declare global {
 
 const connection: mysql.Pool = global._connection || mysql.createPool({
   uri: process.env.JAWSDB_URL || "mysql://kqydyq0eznrb573j:c4ums7k6uohxb5ua@q68u8b2buodpme2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/a8ii54ul8zyr1yt1",
-  connectionLimit: 500,
+  connectionLimit: 12,
 });
 
 global._connection = connection;
