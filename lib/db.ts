308a072aad06922a2c79a07437800818fe944767
Ziv Behar <zivbeh@gmail.com>
2024-10-23 15:27:47 -0700

    new check

--- lib/db.ts
+++ lib/db.ts
@@ -3,9 +3,11 @@ dotenv.config();
 
 import mysql from 'mysql2/promise';
 
+/* eslint-disable no-var */
 declare global {
   var _connection: mysql.Pool | undefined;
 }
+/* eslint-enable no-var */
 
 const connection: mysql.Pool = global._connection || mysql.createPool({
   uri: process.env.JAWSDB_URL || "mysql://kqydyq0eznrb573j:c4ums7k6uohxb5ua@q68u8b2buodpme2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/a8ii54ul8zyr1yt1",
