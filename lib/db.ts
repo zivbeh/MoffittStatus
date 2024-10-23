e99f2587b6f87788be16831056a728c24d4f050b
Ziv Behar <zivbeh@gmail.com>
2024-10-23 15:25:25 -0700

    test db work

--- lib/db.ts
+++ lib/db.ts
@@ -1,22 +1,17 @@
-// db.ts
 import dotenv from 'dotenv';
 dotenv.config();
 
 import mysql from 'mysql2/promise';
 
-let connection: mysql.Pool;
-
 declare global {
   var _connection: mysql.Pool | undefined;
 }
 
-if (!global._connection) {
-  global._connection = mysql.createPool({
-    uri: process.env.JAWSDB_URL || "mysql://kqydyq0eznrb573j:c4ums7k6uohxb5ua@q68u8b2buodpme2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/a8ii54ul8zyr1yt1", // Use your connection string from config vars
-    connectionLimit: 5,          // Adjusted connection limit
-  });
-}
+const connection: mysql.Pool = global._connection || mysql.createPool({
+  uri: process.env.JAWSDB_URL || "mysql://kqydyq0eznrb573j:c4ums7k6uohxb5ua@q68u8b2buodpme2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/a8ii54ul8zyr1yt1",
+  connectionLimit: 500,
+});
 
-connection = global._connection;
+global._connection = connection;
 
 export default connection;
