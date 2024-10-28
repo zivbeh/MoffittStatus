d51d74959bdd6e2d8278e3cdbb1df1f898ef7954
Mason <masonarditi@berkeley.edu>
2024-10-27 21:31:18 -0700

    remove silly stuff

--- lib/db.ts
+++ lib/db.ts
@@ -10,7 +10,7 @@ declare global {
 /* eslint-enable no-var */
 
 const connection: mysql.Pool = global._connection || mysql.createPool({
-  uri: process.env.JAWSDB_URL || "mysql://kqydyq0eznrb573j:c4ums7k6uohxb5ua@q68u8b2buodpme2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/a8ii54ul8zyr1yt1",
+  uri: process.env.JAWSDB_URL ,
   connectionLimit: 12,
 });
 
