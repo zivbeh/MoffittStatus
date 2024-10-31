8d50ffdec8850a162b893ca2f11155a0abb2c536
Ziv Behar <zivbeh@gmail.com>
2024-10-30 19:51:20 -0700

    updated windows hoover effect

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
+      "rounded-xl border bg-card text-card-foreground shadow transition-transform hover:scale-[1.04] lg:hover:scale-[1.03]",
       className
     )}
     {...props}
