196c76e02e6aeef37ce36c5b4d2f08d4526d0d0c
Ziv Behar <zivbeh@gmail.com>
2024-12-06 11:49:25 -0800

    improved metadata

--- app/info/layout.tsx
+++ app/info/layout.tsx
@@ -1,6 +1,8 @@
 // Create a separate file for metadata
 // app/info/metadata.ts
-export const metadata = {
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
     title: 'MoffittStatus - Library Hours | UC Berkeley Libraries',
     description: 'Current operating hours for UC Berkeley Libraries. Check when Moffitt Library and other campus libraries are open.',
     keywords: 'UC Berkeley library hours, Moffitt Library schedule, Berkeley libraries timing',
@@ -14,4 +16,4 @@ export const metadata = {
     alternates: {
       canonical: 'https://moffittstatus.live/info',
     },
-  };
+};
